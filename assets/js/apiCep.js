ef3bcd48bb19398e48685b6c1280f4ff436d61f4
moreira-edu <ml.ri7u@gmail.com>
2022-02-04 18:08:52 -0300

    primeiros testes da api do correio

---
+++ assets/js/apiCep.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+    function limpaFormulario() {
+         $("#cep").val(``);
+         $("#rua").val(``);
+         $("#bairro").val(``);
+         $("#cidade").val(``);
+        $("#uf").val(``);
+    }
+    $("#cep").blur(function () {
+        var cep = $(this).val().replace(/[^\d]/g,' ');
+        if (cep != '') {
+            var validaCep = /^[0-9]{8}$/;
+            var correios = `https://viacep.com.br/ws/${cep}/json/?`;
+            if (validaCep.test(cep)) {
+                $("#cep").val(`...`);
+                $("#rua").val(`...`);
+                $("#bairro").val(`...`);
+                $("#cidade").val(`...`);
+               $("#uf").val(`...`);
+
+                $.ajax({
+                    url: correios,
+                    success: function (dados) {
+                        $("#cep").val(dados.cep);
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                       $("#uf").val(dados.uf);
+                    },
+                    error: function () {
+                        limpaFormulario();
+                        alert(`CEP inválido`);
+                    }
+                });
+            } else {
+                limpaFormulario();
+            }
+        }
+    });
+
+});
