6429e351d6a2393ce61b2760b8cfafb8f9400ca6
moreira-edu <ml.ri7u@gmail.com>
2022-02-06 22:21:22 -0300

    commit de saída

--- assets/js/cadastroCliente.js
+++ assets/js/cadastroCliente.js
@@ -17,37 +17,48 @@ class cliente {
     }
 }
 
-class gerenciador{
-    constructor(){
-        this.contas = [];
-    }
+// class gerenciador{
+//     constructor(){
+//         this.contas = [];
+//     }
 
-    entrada(...conta){
-        for(let i =0;i<conta.length; i++){
-            this.contas.push(conta[i])
-        }
-    }
+//     entrada(...conta){
+//         for(let i =0;i<conta.length; i++){
+//             this.contas.push(conta[i])
+//         }
+//     }
+// }
+
+// function cadastrar() {
+
+//     const cliente1 = new cliente();
+//     cliente1.nome = $("#form-nome").val();
+//     cliente1.email = $("#form-email").val();
+//     cliente1.usuario = $("#form-usuario").val();
+//     cliente1.senha = $("#form-senha").val();
+
+
+
+//     localStorage.setItem('conta', JSON.stringify(cliente1));
+//     teste = JSON.parse(localStorage.getItem('conta'));
+//     console.log(teste);
+//     console.log(teste._nome);
+//     console.log(teste._email);
+//     console.log(teste._usuario);
+//     console.log(teste._senha);
+
+// }
+
+
+var emailOk = function (value) {
+
+    let ok = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+    return ok.test(value);
+}
+var senhaForte = function(value){
+    let ok =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
+    return ok.test(value);
 }
 
-
-
-
-function cadastrar() {
-     
-    let clienteNome=$("#form-nome").val();
-    let clienteEmail=$("#form-email").val();
-    let clienteUsuario=$("#form-usuario").val();
-    let clienteSenha= $("#form-senha").val();
-
-    const cliente1 = new cliente();
-    cliente1.nome = clienteNome;
-    cliente1.email = clienteEmail;
-    cliente1.usuario = clienteUsuario;
-    cliente1.senha = clienteSenha;
-    let conta = new gerenciador();
-    
-   
-    localStorage.setItem('conta', JSON.stringify(cliente1));
-    conta.entrada(JSON.parse(localStorage.getItem('conta')));
-    console.log(conta);
-}
+console.log(emailOk('eduardo@email.com'));
+console.log(senhaForte('eduzTaou@1234'));
