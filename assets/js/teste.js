4a2758c0c9b52170b4a3724b160067f56dda6fb8
moreira-edu <ml.ri7u@gmail.com>
2022-02-08 13:12:52 -0300

    finalização de funções básicas de controle do formulário

--- assets/js/teste.js
+++ assets/js/teste.js
@@ -1,40 +1,58 @@
 $(document).ready(function(){
     $("#form-submit").attr('disabled', true);
+    $("#form-senha").attr('disabled', true);
+    $("#form-senha2").attr('disabled', true);
         $("#form-email").keyup(function(){
             let emailOk = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
             let email = $("#form-email").val();
             emailOk.test(email) ?(
                 $("#msg-email").text(`e-mail válido`),
-                $("#img-email").attr('src', 'assets/icons/verificado.png')             
+                $("#img-email").attr('src', 'assets/icons/verificado.png'),
+                $("#form-email").addClass('certo'),
+                $("#form-senha").attr('disabled', false)              
               ) : (
                 $("#msg-email").text(`e-mail inválido`),
                 $("#img-email").attr('src', 'assets/icons/cancelar.png'),
-                $("#form-email").addClass('erro')              
+                $("#form-email").addClass('erro'),
+                $("#form-senha").attr('disabled', true)             
               );
-        })
+        });
         $("#form-senha").keyup(function(){
             let senha = $("#form-senha").val();
             let senhaForte = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
             senhaForte.test(senha) ?(
                 $("#msg-senha").text(`senha forte`),
-                $("#img-senha").attr('src', 'assets/icons/verificado.png')
+                $("#img-senha").attr('src', 'assets/icons/verificado.png'),
+                $("#form-senha").addClass('certo'),
+                $("#form-senha2").attr('disabled', false)
             ) :(
                 $("#msg-senha").text(`Insira uma senha forte (ex: seNha@1234)`),
-                $("#img-senha").attr('src', 'assets/icons/cancelar.png')
+                $("#img-senha").attr('src', 'assets/icons/cancelar.png'),
+                $("#form-senha").addClass('erro'),
+                $("#form-senha2").attr('disabled', true)
             )            
         });
         $("#form-senha2").keyup(function(){
+            if(!$("#form-senha2").val()==''){
             let senha = $("#form-senha").val();
             let senha2 = $("#form-senha2").val();
            senha == senha2 ?(
                 $("#msg-senha2").text(`senhas coincidem`),
                 $("#form-submit").attr('disabled', false),
-                $("#img-senha2").attr('src', 'assets/icons/verificado.png')
+                $("#img-senha2").attr('src', 'assets/icons/verificado.png'),
+                $("#form-senha2").addClass('certo')
            ): (
             $("#msg-senha2").text(`Por favor, digite a mesma senha`),
             $("#form-submit").attr('disabled', true),
-            $("#img-senha2").attr('src', 'assets/icons/cancelar.png')
+            $("#img-senha2").attr('src', 'assets/icons/cancelar.png'),
+            $("#form-senha2").addClass('erro')
            )
+           } else{
+            $("#msg-senha2").text(`Por favor, digite a mesma senha`);
+            $("#img-senha2").attr('src', 'assets/icons/cancelar.png');
+            $("#form-senha2").addClass('erro');
+            $("#form-submit").attr('disabled', true);
+           }
         });
 
         $("#mostraSenha").on('click', function(){
