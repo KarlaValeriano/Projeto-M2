59db1c9b2f07c1d52d26dbf3733eeb660fe9e897
moreira-edu <ml.ri7u@gmail.com>
2022-02-07 22:56:20 -0300

    controle de formulário pronto

--- assets/js/cadastroCliente.js
+++ assets/js/cadastroCliente.js
@@ -7,58 +7,62 @@ class cliente {
         this._nome = value
     }
     set email(value) {
+        let ok = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+        if(ok.test(value) == true){
+            $("#msg-email").text('')
         this._email = value;
+        }else{
+            $("#msg-email").text(`Por favor, digite um e-mail válido`);
+        }
     }
     set usuario(value) {
         this._usuario = value;
     }
     set senha(value) {
-        this._senha = value;
+        let ok = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/gi;
+        if(ok.test(value) == true){
+            $("#msg-senha").text(``);           
+            this._senha = value;
+        } else{
+            $("#msg-senha").text(`Por favor, digite uma senha forte (letra maiúscula, caractere especial e números)`);           
+        }
+        
     }
 }
 
-// class gerenciador{
-//     constructor(){
-//         this.contas = [];
-//     }
+function cadastrar() {
+    let senhaForte = 
 
-//     entrada(...conta){
-//         for(let i =0;i<conta.length; i++){
-//             this.contas.push(conta[i])
-//         }
-//     }
-// }
-
-// function cadastrar() {
-
-//     const cliente1 = new cliente();
-//     cliente1.nome = $("#form-nome").val();
-//     cliente1.email = $("#form-email").val();
-//     cliente1.usuario = $("#form-usuario").val();
-//     cliente1.senha = $("#form-senha").val();
+    // const cliente1 = new cliente();    
+    // cliente1.nome = $("#form-nome").val();
+    // cliente1.email = $("#form-email").val();
+    // cliente1.usuario = $("#form-usuario").val();
+    // cliente1.senha = $("#form-senha").val();
 
 
 
-//     localStorage.setItem('conta', JSON.stringify(cliente1));
-//     teste = JSON.parse(localStorage.getItem('conta'));
+    localStorage.setItem('conta', JSON.stringify(cliente1));    
+
+}
+
+// teste = JSON.parse(localStorage.getItem('conta'));
 //     console.log(teste);
 //     console.log(teste._nome);
 //     console.log(teste._email);
 //     console.log(teste._usuario);
 //     console.log(teste._senha);
 
+
+
+// var emailOk = function () {
+//     let email = $("#form-email").val();
+//     let ok = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+//     return ok.test(email);
+// }
+// var senhaForte = function(value){
+//     let ok =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
+//     return ok.test(value);
 // }
 
-
-var emailOk = function (value) {
-
-    let ok = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
-    return ok.test(value);
-}
-var senhaForte = function(value){
-    let ok =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
-    return ok.test(value);
-}
-
-console.log(emailOk('eduardo@email.com'));
+// console.log(emailOk('eduardo@email.com'));
 console.log(senhaForte('eduzTaou@1234'));
---
+++ assets/js/teste.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $("#form-submit").attr('disabled', true);
+        $("#form-email").keyup(function(){
+            let emailOk = (/^([a-zA-Z][^<>\"!@[\]#$%¨&*()~^:;ç,\-´`=+{}º\|/\\?]{1,})@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+            let email = $("#form-email").val();
+            emailOk.test(email) ?(
+                $("#msg-email").text(`e-mail válido`),
+                $("#img-email").attr('src', 'assets/icons/verificado.png')             
+              ) : (
+                $("#msg-email").text(`e-mail inválido`),
+                $("#img-email").attr('src', 'assets/icons/cancelar.png'),
+                $("#form-email").addClass('erro')              
+              );
+        })
+        $("#form-senha").keyup(function(){
+            let senha = $("#form-senha").val();
+            let senhaForte = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$/;
+            senhaForte.test(senha) ?(
+                $("#msg-senha").text(`senha forte`),
+                $("#img-senha").attr('src', 'assets/icons/verificado.png')
+            ) :(
+                $("#msg-senha").text(`Insira uma senha forte (ex: seNha@1234)`),
+                $("#img-senha").attr('src', 'assets/icons/cancelar.png')
+            )            
+        });
+        $("#form-senha2").keyup(function(){
+            let senha = $("#form-senha").val();
+            let senha2 = $("#form-senha2").val();
+           senha == senha2 ?(
+                $("#msg-senha2").text(`senhas coincidem`),
+                $("#form-submit").attr('disabled', false),
+                $("#img-senha2").attr('src', 'assets/icons/verificado.png')
+           ): (
+            $("#msg-senha2").text(`Por favor, digite a mesma senha`),
+            $("#form-submit").attr('disabled', true),
+            $("#img-senha2").attr('src', 'assets/icons/cancelar.png')
+           )
+        });
+});
