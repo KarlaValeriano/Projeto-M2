8aa74a7891e58a14ed599af161f12d0e36805279
moreira-edu <ml.ri7u@gmail.com>
2022-02-13 13:31:52 -0300

    alterações na no comportamento do formulario de login e carrocel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,28 @@
-$("#login").on('click',function(event){
-    event.preventDefault();
-    $(".container-user").removeClass('invisivel');
-    $(".form-login").addClass('invisivel');
+$(document).ready(function () {
+    let usuario = new GerenciadorUsuario();
+    teste = usuario.buscaUsuario();
+    usuario.chamar();
+    $("#saudacao-user").text(`Olá ${teste._nome}`);
+
+    teste == '' ? (
+        $("user-btn").text(`Login`),
+        $(".container-user").addClass('invisivel'),
+        $(".form-login").removeClass('invisivel')
+       
+    ) : (
+        $(".user-btn").text(`Olá ${teste._nome}`),
+        $(".container-user").removeClass('invisivel'),
+        $(".form-login").addClass('invisivel')
+    )
+
+    $("#login").on('click', function () {
+        usuario.entrar();
+    });
+    $("#logout").on('click', function () {
+        usuario.sair();
+    });
 });
 
-$("#logout").on('click', function(){
-    $(".container-user").addClass('invisivel');
-    $(".form-login").removeClass('invisivel');
-});
+
+
+
--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -1,7 +1,26 @@
-class gerenciadorUsuario{
-
-    buscaUsuario(){
-        return JSON.parse(localStorage.getItem('conta'));
+class GerenciadorUsuario {
+    constructor() {
+        this.teste;
+    }
+    buscaUsuario() {
+        this.teste = JSON.parse(localStorage.getItem('conta'));
+        return this.teste;
+    }
+    entrar() {
+        let ok = this.teste._email == $("#user-email").val() && this.teste._senha == $("#user-senha").val();               
+        ok == true ? (
+        $(".container-user").removeClass('invisivel'),
+        $(".form-login").addClass('invisivel')
+        ) : (
+        $("#msg-user").text(`E-mail ou senha inválido`)
+        )
+    }
+    sair() {
+        $(".container-user").addClass('invisivel');
+        $(".form-login").removeClass('invisivel');
+    }
+    chamar() {
+        console.log(this.teste._email);
     }
 }
 
